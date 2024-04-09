1b1428fa9d53d86733aa6443bf6eb8ababbe5586
eliooooooo <eliott.burkle@gmail.com>
2024-04-09 11:37:48 +0200

    Feat(dl): button to dl package.json on click

--- public/assets/js/copy.js
+++ public/assets/js/copy.js
@@ -18,4 +18,28 @@ document.addEventListener('DOMContentLoaded', function() {
             copyToClipboard('#install');
         });
     }
+
+    let dl = document.querySelector('.button-dl');
+
+    if (dl) {
+        dl.addEventListener('click', function() {
+            // Récupérer le contenu de la balise
+            let content = document.querySelector('.content').innerText;
+
+            // Créer un nouveau Blob avec le contenu
+            let blob = new Blob([content], { type: 'text/plain' });
+
+            // Créer un nouveau lien avec le Blob comme source
+            let url = URL.createObjectURL(blob);
+            let link = document.createElement('a');
+            link.href = url;
+            link.download = 'package.json';
+
+            document.body.appendChild(link);
+            link.click();
+
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+        });
+    }
 });
